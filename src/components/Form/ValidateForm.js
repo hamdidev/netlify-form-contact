f6c0219491c62d6cfd940813ffe39a98744b35c3
hamdidev <itechtranss77@gmail.com>
2022-04-07 20:09:25 -0700

    form done

---
+++ src/components/Form/ValidateForm.js
@@ -0,0 +1,18 @@
+export default function validateForm({ name, email, message }) {
+  if (!name.trim()) {
+    return "Username required";
+  }
+
+  const regex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!email) {
+    return "Email required";
+  } else if (regex.test(email.toLocalLowerCase)) {
+    return "Email address is invalid";
+  }
+
+  if (!message) {
+    return "Message subject is required";
+  }
+  return null;
+}
